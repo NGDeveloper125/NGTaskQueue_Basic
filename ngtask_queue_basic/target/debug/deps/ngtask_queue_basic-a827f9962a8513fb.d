8ffd597cdc98018f450e22971d099f49ffbf7fc8
NGDeveloper125 <NGDeveloper125@outlook.com>
2025-04-26 14:56:15 +0100

    Update toml file for publishing crate

---
+++ ngtask_queue_basic/target/debug/deps/ngtask_queue_basic-a827f9962a8513fb.d
@@ -0,0 +1,5 @@
+/home/ngdeveloper/NGTaskQueue_Basic/ngtask_queue_basic/target/debug/deps/libngtask_queue_basic-a827f9962a8513fb.rmeta: src/lib.rs
+
+/home/ngdeveloper/NGTaskQueue_Basic/ngtask_queue_basic/target/debug/deps/ngtask_queue_basic-a827f9962a8513fb.d: src/lib.rs
+
+src/lib.rs:
---
+++ ngtask_queue_basic/target/debug/deps/ngtask_queue_basic-cb952abd18b3b220.d
@@ -0,0 +1,5 @@
+/home/ngdeveloper/NGTaskQueue_Basic/ngtask_queue_basic/target/debug/deps/libngtask_queue_basic-cb952abd18b3b220.rmeta: src/lib.rs
+
+/home/ngdeveloper/NGTaskQueue_Basic/ngtask_queue_basic/target/debug/deps/ngtask_queue_basic-cb952abd18b3b220.d: src/lib.rs
+
+src/lib.rs:
